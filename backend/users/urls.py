d4a38733e9e8d7804d762bfa5d655b9dc6c5d790
Azamat Uzbekov <batukasss@gmail.com>
2024-11-26 11:46:56 -0600

    Add profile view and password reset functionality; update navigation and templates

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.Home, name='home'),
-    path('register/', views.RegisterView, name='register'),
+    path('forgot-password/', views.ForgotPassword, name='forgot-password'),
     path('login/', views.LoginView, name='login'),
     path('logout/', views.LogoutView, name='logout'),
-    path('forgot-password/', views.ForgotPassword, name='forgot-password'),
     path('password-reset-sent/<str:reset_id>/', views.PasswordResetSent, name='password-reset-sent'),
+    path('profile/', views.profile, name='profile'),
+    path('register/', views.RegisterView, name='register'),
     path('reset-password/<str:reset_id>/', views.ResetPassword, name='reset-password'),
 ]
